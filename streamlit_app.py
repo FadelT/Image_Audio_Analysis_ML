872d1c6ea2f9dcbe2eb5bd31f5aa04101a03065d
Nbouyaa <67773915+FadelT@users.noreply.github.com>
2022-05-21 16:52:31 +0200

    nthg

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ import boto3
 from dotenv import dotenv_values
 
 config = dotenv_values(".env") 
-aws_access_key_id=config['AKIAQCDOW6QYJPXAU7LM']
+aws_access_key_id=config['AWS_ACCESS_ID']
 aws_secret_access_key=config['AWS_ACCESS_KEY']
 api_key=config['ASSEMBLY_KEY']
 
